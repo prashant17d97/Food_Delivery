4884eb1ef73f5658eea21e7bfd7373be704a59d3
prashant17d97 <84988691+prashant17d97@users.noreply.github.com>
2022-12-29 15:46:05 +0530

    Initials

---
+++ app/src/main/java/com/prashant/fooddelivery/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.prashant.fooddelivery.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryLight = Color(0xFFFB7500)
+val lightBackground = Color(0xFFEEF2F5)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+//Dark
+val primaryDark = Color(0xFFFB7500)
+val darkBackground = Color(0xFF353E40)
