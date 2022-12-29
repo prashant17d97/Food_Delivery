bdfaba7cead7d3b17ff821056977a4a7190a83aa
prashant17d97 <84988691+prashant17d97@users.noreply.github.com>
2022-12-29 18:13:30 +0530

    Splash

--- app/src/main/java/com/prashant/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/prashant/fooddelivery/MainActivity.kt
@@ -3,39 +3,60 @@ package com.prashant.fooddelivery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.prashant.fooddelivery.ui.theme.FoodDeliveryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-//        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContent {
-            FoodDeliveryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+        installSplashScreen().apply {
+            this.setOnExitAnimationListener { viewProvider ->
+                viewProvider.iconView
+                    .animate()
+                    .setDuration(500L)
+                    .alpha(0f)
+                    .withEndAction {
+                        viewProvider.remove()
+                        setContent {
+                            FoodDeliveryTheme {
+                                // A surface container using the 'background' color from the theme
+                                Surface(
+                                    modifier = Modifier.fillMaxSize(),
+                                    color = MaterialTheme.colors.background
+                                ) {
+                                    Greeting("Android")
+                                }
+                            }
+                        }
+                    }
+
+                    .start()
             }
         }
+
+
     }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-    Image(painter = painterResource(id = R.drawable.main_logo), contentDescription ="")
+    Image(painter = painterResource(id = R.drawable.main_logo), contentDescription = "")
 }
 
 @Preview(showBackground = true)
